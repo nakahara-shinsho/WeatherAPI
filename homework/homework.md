8cba4299269558ef55749ef44807cf6c840448e3
Xinxiao-Li <starsmall.xiaoqq@gmail.com>
2024-12-09 15:22:04 +0900

    add homework

---
+++ homework/homework.md
@@ -0,0 +1,26 @@
+
+# 課題１： 郵便番号に指定する場所の天気情報を取得する
+- url = "https://api.openweathermap.org/data/2.5/weather?zip={郵便番号}&units=metric&appid={API_key}"
+
+
+- {郵便番号}
+  - 例えば："zip=110-0006,JP", 
+
+- API_key:
+  - あなたのAPI KEY
+
+# 課題2： 郵便番号に指定する場所の天気情報を取得する
+- url = "https://api.openweathermap.org/data/2.5/weather?lat={緯度}&lon={経度}&units=metric&appid={API_key}"
+
+
+- {経度}(lat)・{緯度}（lon）の値：
+  - Google Mapから右クリックすることで得られます
+  - 例えば："lat=35.2932718&lon=139.97671"
+
+- API_key:
+  - あなたのAPI KEY
+
+
+# 次回の課題予告：　下記のように設定画面で書いて、5分間、一日のデータグラフを作成する。
+ 
+![alt text](image-1.png)
---
+++ tamaWeather.js
@@ -0,0 +1,67 @@
+//openweathermap（天気予報API）に接続
+var cityName = "tama";　//都市名
+var owmApiKey = "438531953753d6e4bb36354a22ba948a"; //APIキー
+var owmURL = "http://api.openweathermap.org/data/2.5/weather?q="+ cityName +",jp&lang=ja&APPID="+ owmApiKey +""; //URL
+
+//デバッグ用にコンソールにURLを表示
+console.log(owmURL);
+
+//htmlのtbody要素を呼び出し
+tblBody = document.getElementsByTagName("tbody")[0]
+
+//初めの「値：」表示セル
+var messageElement = document.createElement("td"); //セルを作成
+messageElement.setAttribute("class", "fixed-column-0 table-light") //クラスを設定
+messageElement.innerHTML = "値：" //テキストを設定
+tblBody.append(messageElement);  //セルを追加
+
+//天気情報を格納するセル
+fetch(owmURL)
+  .then(response => {
+    return response.json();  //結果データをJSON形式に変換
+  })
+  .then(data => {
+    console.log(data); //データをコンソールに表示
+    messageElement = document.createElement("td"); //セルを作成
+    messageElement.setAttribute("class", "fixed-column-1 table-light") //クラスを設定
+    messageElement.textContent  = data["main"].temp ; //温度
+    tblBody.append(messageElement); //セルを追加
+
+    messageElement = document.createElement("td");
+    messageElement.setAttribute("class", "fixed-column-2")
+    messageElement.textContent  = data["main"].feels_like; //体感温度
+    tblBody.append(messageElement);
+
+    messageElement = document.createElement("td");
+    messageElement.setAttribute("class", "fixed-column-3")
+    messageElement.textContent  = data["main"].humidity; //湿度
+    tblBody.append(messageElement);
+
+    messageElement = document.createElement("td");
+    messageElement.setAttribute("class", "fixed-column-4")
+    messageElement.textContent  = data["main"].pressure; //気圧
+    tblBody.append(messageElement);
+
+    messageElement = document.createElement("td");
+    messageElement.setAttribute("class", "fixed-column-5")
+    messageElement.textContent  = data["wind"].speed; //風速
+    tblBody.append(messageElement);
+
+    messageElement = document.createElement("td");
+    messageElement.setAttribute("class", "fixed-column-6")
+    messageElement.textContent  = data["clouds"].all + " ("+　data["weather"][0].description + ")"; //雲量
+    tblBody.append(messageElement);
+
+    messageElement = document.createElement("td");
+    messageElement.setAttribute("class", "fixed-column-7")
+    messageElement.textContent  = ("rain" in data) ? data["rain"]: "--"; //降水量
+    tblBody.append(messageElement);
+
+    messageElement = document.createElement("td");
+    messageElement.setAttribute("class", "fixed-column-8")
+    messageElement.textContent  = ("snow" in data) ? data["snow"]: "--"; //積雪量
+    tblBody.append(messageElement);
+  })
+  .catch(error => {
+    console.error(error);  
+  });
