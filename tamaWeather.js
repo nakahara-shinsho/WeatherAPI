44a77294897ae45a81ce78a2ffec825810cf8e9d
Xinxiao-Li <starsmall.xiaoqq@gmail.com>
2024-12-09 15:40:21 +0900

    update table color

--- tamaWeather.js
+++ tamaWeather.js
@@ -10,7 +10,7 @@ console.log(owmURL);
 tblBody = document.getElementsByTagName("tbody")[0]
 
 //初めの「値：」表示セル
-var messageElement = document.createElement("td"); //セルを作成
+var messageElement = document.createElement("th"); //セルを作成
 messageElement.setAttribute("class", "fixed-column-0 table-light") //クラスを設定
 messageElement.innerHTML = "値：" //テキストを設定
 tblBody.append(messageElement);  //セルを追加
@@ -22,42 +22,42 @@ fetch(owmURL)
   })
   .then(data => {
     console.log(data); //データをコンソールに表示
-    messageElement = document.createElement("td"); //セルを作成
+    messageElement = document.createElement("th"); //セルを作成
     messageElement.setAttribute("class", "fixed-column-1 table-light") //クラスを設定
     messageElement.textContent  = data["main"].temp ; //温度
     tblBody.append(messageElement); //セルを追加
 
-    messageElement = document.createElement("td");
+    messageElement = document.createElement("th");
     messageElement.setAttribute("class", "fixed-column-2")
     messageElement.textContent  = data["main"].feels_like; //体感温度
     tblBody.append(messageElement);
 
-    messageElement = document.createElement("td");
+    messageElement = document.createElement("th");
     messageElement.setAttribute("class", "fixed-column-3")
     messageElement.textContent  = data["main"].humidity; //湿度
     tblBody.append(messageElement);
 
-    messageElement = document.createElement("td");
+    messageElement = document.createElement("th");
     messageElement.setAttribute("class", "fixed-column-4")
     messageElement.textContent  = data["main"].pressure; //気圧
     tblBody.append(messageElement);
 
-    messageElement = document.createElement("td");
+    messageElement = document.createElement("th");
     messageElement.setAttribute("class", "fixed-column-5")
     messageElement.textContent  = data["wind"].speed; //風速
     tblBody.append(messageElement);
 
-    messageElement = document.createElement("td");
+    messageElement = document.createElement("th");
     messageElement.setAttribute("class", "fixed-column-6")
     messageElement.textContent  = data["clouds"].all + " ("+　data["weather"][0].description + ")"; //雲量
     tblBody.append(messageElement);
 
-    messageElement = document.createElement("td");
+    messageElement = document.createElement("th");
     messageElement.setAttribute("class", "fixed-column-7")
     messageElement.textContent  = ("rain" in data) ? data["rain"]: "--"; //降水量
     tblBody.append(messageElement);
 
-    messageElement = document.createElement("td");
+    messageElement = document.createElement("th");
     messageElement.setAttribute("class", "fixed-column-8")
     messageElement.textContent  = ("snow" in data) ? data["snow"]: "--"; //積雪量
     tblBody.append(messageElement);
